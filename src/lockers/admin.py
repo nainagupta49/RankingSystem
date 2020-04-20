2f47f7f79d454e3dcd0ba008c62b9fc0b5dac1f4
VRINDA GOYAL <vrinda22@users.noreply.github.com>
2020-04-21 01:09:24 +0530

    db created

---
+++ src/lockers/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Onboard
+from .models import Rating
+from .models import Throughput
+from .models import Availability
+from .models import Occupancy
+
+admin.site.register(Onboard)
+admin.site.register(Rating)
+admin.site.register(Throughput)
+admin.site.register(Availability)
+admin.site.register(Occupancy)
